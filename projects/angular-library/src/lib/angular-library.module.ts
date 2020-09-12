039930b54fa65decfaa09794d9e73959c19b309a
Cristian Quispe Ramirez <cristian.quispe@globant.com>
2020-09-12 08:44:17 -0500

    create library
    ng g library angular-library

---
+++ projects/angular-library/src/lib/angular-library.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AngularLibraryComponent } from './angular-library.component';
+
+
+
+@NgModule({
+  declarations: [AngularLibraryComponent],
+  imports: [
+  ],
+  exports: [AngularLibraryComponent]
+})
+export class AngularLibraryModule { }
